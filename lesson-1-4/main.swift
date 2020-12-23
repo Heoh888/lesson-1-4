ce8845111c803ac9779af2a6baf27670416bd8fc
alexfit <heoh888@ya.ru>
2020-12-23 20:07:57 +0300

    add gitignore

--- lesson-1-4/main.swift
+++ lesson-1-4/main.swift
@@ -1,85 +1,125 @@
-//1. Описать класс Car c общими свойствами автомобилей и пустым методом действия по аналогии с прошлым заданием.
-
-//2. Описать пару его наследников trunkCar и sportСar. Подумать, какими отличительными свойствами обладают эти автомобили. Описать в каждом наследнике специфичные для него свойства.
-
-//3. Взять из прошлого урока enum с действиями над автомобилем. Подумать, какие особенные действия имеет trunkCar, а какие – sportCar. Добавить эти действия в перечисление.
-
-//4. В каждом подклассе переопределить метод действия с автомобилем в соответствии с его классом.
-
-//5. Создать несколько объектов каждого класса. Применить к ним различные действия.
-
-//6. Вывести значения свойств экземпляров в консоль.
 
 import Foundation
-
-enum TrunkCarSettings{
-    case download, upload
+// Возможные действия с автомобилем
+enum TrunkCarIllumination: String{
+    case enabled = "Включена"
+    case disabled = "Выключена"
 }
-
-enum SportCarSettings: String{
+enum SportCarSpoiler: String{
      case raised = "Поднят"
      case retracted = "Убран"
 }
-
 enum EngineStatus: String {
     case started = "Заведен"
     case muted = "Заглушен"
 }
-
+// Общий класс автомобилей
 class Car{
     let carBrand: String
     let carColor: String
     let yearRelease: Int
-    let weightCar: Double // вес авто
+    let carNumber: String
     var engine: EngineStatus
-    init(carBrand: String, carColor: String, yearRelease: Int, weightCar: Int, engine: EngineStatus ) {
+    init(carBrand: String, carColor: String, yearRelease: Int, carNumber: String, engine: EngineStatus ) {
         self.carBrand = carBrand
         self.carColor = carColor
+        self.carNumber = carNumber
         self.yearRelease = yearRelease
-        self.weightCar = Double(weightCar)
         self.engine = engine
     }
-}
-
-class TrunkCar: Car {
-
-}
-
-class SportСar: Car{
-    let enginePower: Double
-    var spoiler: SportCarSettings
-    init(carBrand: String, carColor: String, yearRelease: Int, weightCar: Int, engine: EngineStatus, spoiler: SportCarSettings, enginePower: Int) {
-        self.spoiler = spoiler
-        self.enginePower = Double(enginePower)
-        super.init(carBrand: carBrand, carColor: carColor, yearRelease: yearRelease, weightCar: weightCar, engine: engine)
+    func autoData() -> String {
+        return "\(self.carNumber)"
     }
-    func overclockingTime() -> String{
-        let greeting = weightCar/enginePower
-        let time = (greeting*100).rounded()/100
+}
+// Класс наследник TrunkCar от класса Car
+class TrunkCar: Car {
+    let bodyVolume: Double
+    var loadedBodyVolume: Double
+    let detailsOfTheCarOwner: String
+    var illumination: TrunkCarIllumination
+    init(carBrand: String, carColor: String, yearRelease: Int, carNumber: String, engine: EngineStatus, bodyVolume: Int, loadedBodyVolume: Int, illumination: TrunkCarIllumination, detailsOfTheCarOwner: String) {
+        self.bodyVolume = Double(bodyVolume)
+        self.loadedBodyVolume = Double(loadedBodyVolume)
+        self.illumination = illumination
+        self.detailsOfTheCarOwner = detailsOfTheCarOwner
+        super.init(carBrand: carBrand, carColor: carColor, yearRelease: yearRelease, carNumber: carNumber, engine: engine)
+    }
+    // Функция расчета заполненность кузова
+    func filledBody() -> String{
+        let volumePercentage = (loadedBodyVolume/bodyVolume*100*100).rounded()/100
         var messages: String = ""
-        if engine == .started{
-            messages = "Разгон от 0 до 100 км за \(time) секунд"
-        }else if engine == .muted {
-            messages = "Заведите машну!"
+        if volumePercentage > 0{
+            messages = "Кузов заполнен на \(volumePercentage) процентов"
+        }else if volumePercentage == 0{
+            messages = "Кузов пуст"
+        }else if bodyVolume < loadedBodyVolume{
+            messages = "Кузов переполнен"
         }
         return messages
     }
-    func printConsol(){
-        print("Марка авто - \(carBrand), Цвет авто - \(carColor), Год выпуска - \(yearRelease), Вес авто - \(weightCar) кг., Статус двигателя - \(engine.rawValue), Мощность двигателя - \(enginePower) лс., Статус сполера - \(spoiler.rawValue), \(overclockingTime())")
+    override func autoData() -> String {
+        return "Номер авто - \(self.carNumber) Название компании - \(self.detailsOfTheCarOwner)"
     }
-    func SettingAuto (spoiler: SportCarSettings, engine: EngineStatus){
+    func printConsol(){
+        print("Марка авто - \(carBrand), Цвет авто - \(carColor), Год выпуска - \(yearRelease), \(autoData()), Статус двигателя - \(engine.rawValue), Объем кузова - \(bodyVolume) м³, \(filledBody()), Иллюминация - \(illumination.rawValue).")
+    }
+    func SettingAuto (loadedBodyVolume: Int, illumination: TrunkCarIllumination, engine: EngineStatus){
+        self.loadedBodyVolume = Double(loadedBodyVolume)
+        self.illumination = illumination
+        self.engine = engine
+    }
+}
+// Класс наследник SportСar от класса Car
+class SportСar: Car{
+    let enginePower: Double
+    let weightCar: Double
+    let detailsOfTheCarOwner: String
+    var spoiler: SportCarSpoiler
+    init(carBrand: String, carColor: String, yearRelease: Int, carNumber: String, engine: EngineStatus, weightCar: Double, spoiler: SportCarSpoiler, enginePower: Int, detailsOfTheCarOwner: String) {
+        self.spoiler = spoiler
+        self.weightCar = Double(weightCar)
+        self.enginePower = Double(enginePower)
+        self.detailsOfTheCarOwner = detailsOfTheCarOwner
+        super.init(carBrand: carBrand, carColor: carColor, yearRelease: yearRelease, carNumber: carNumber, engine: engine)
+    }
+    // Функция определения разгона от 0 до 100 км
+    func overclockingTime() -> String{
+        let time = (weightCar/enginePower*100).rounded()/100
+        var messages: String = ""
+        if engine == .started{
+            messages = "Разгон от 0 до 100 км за \(time) секунд."
+        }else if engine == .muted {
+            messages = "Заведите машину!"
+        }
+        return messages
+    }
+    override func autoData() -> String {
+        return "Номер авто - \(self.carNumber) Ф.И.О владельца - \(self.detailsOfTheCarOwner)"
+    }
+    func printConsol(){
+        print("Марка авто - \(carBrand), Цвет авто - \(carColor), Год выпуска - \(yearRelease), \(autoData()), Вес авто - \(weightCar) кг., Статус двигателя - \(engine.rawValue), Мощность двигателя - \(enginePower) лс., Статус споллера - \(spoiler.rawValue), \(overclockingTime())")
+    }
+    func SettingAuto (spoiler: SportCarSpoiler, engine: EngineStatus){
         self.engine = engine
         self.spoiler = spoiler
     }
 }
 
+//  Инициализируем экземпляры
+var lotus = SportСar(carBrand: "Lotus", carColor: "Белый", yearRelease: 2018, carNumber: "м567ар777", engine: .muted,  weightCar: 1550, spoiler: .retracted, enginePower: 360, detailsOfTheCarOwner: "Жуков Анатолий Иванович")
+lotus.SettingAuto(spoiler: .raised, engine: .started)
 
+var ferari = SportСar(carBrand: "Ferari", carColor: "Красный", yearRelease: 2019, carNumber: "г010ар797", engine: .muted, weightCar: 1750, spoiler: .retracted, enginePower: 460, detailsOfTheCarOwner: "Петров Василий Кротов")
+// ferari.SettingAuto(spoiler: .raised, engine: .started)
 
-var lotus = SportСar(carBrand: "Lotus", carColor: "Белый", yearRelease: 2018, weightCar: 1550, engine: .started, spoiler: .raised, enginePower: 360)
-lotus.SettingAuto(spoiler: .retracted, engine: .muted)
+var volvo = TrunkCar(carBrand: "Volvo", carColor: "Желтый", yearRelease: 2017, carNumber: "в865ру77", engine: .muted, bodyVolume: 75, loadedBodyVolume: 0, illumination: .disabled, detailsOfTheCarOwner: "ООО Доставим  Быстро")
+volvo.SettingAuto(loadedBodyVolume: 22, illumination: .enabled, engine: .started)
 
-var ferari = SportСar(carBrand: "Ferari", carColor: "Красный", yearRelease: 2019, weightCar: 1750, engine: .started, spoiler: .raised, enginePower: 460)
+var kamaz = TrunkCar(carBrand: "Камаз", carColor: "Черный", yearRelease: 2020, carNumber: "к432ло99", engine: .muted, bodyVolume: 75, loadedBodyVolume: 0, illumination: .disabled, detailsOfTheCarOwner: "ЗАО ТрансКонтинент")
+//kamaz.SettingAuto(loadedBodyVolume: 22, illumination: .enabled, engine: .started)
 
-
+// Распечатаем в консоль
 lotus.printConsol()
 ferari.printConsol()
+volvo.printConsol()
+kamaz.printConsol()
