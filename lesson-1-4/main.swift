18497ed0e21b4f0d6650356abeaed03f70f215fd
alexfit <heoh888@ya.ru>
2020-12-23 03:45:57 +0300

    Described the class SportСar

--- lesson-1-4/main.swift
+++ lesson-1-4/main.swift
@@ -13,47 +13,73 @@
 import Foundation
 
 enum TrunkCarSettings{
-    case download, upload // Статус кузова (Загружен / Выгружен)
-    
+    case download, upload
 }
 
-enum SportCarSettings{
-     case raised, retracted // Статус спойлера (Поднят / Убран)
+enum SportCarSettings: String{
+     case raised = "Поднят"
+     case retracted = "Убран"
 }
 
-enum EngineStatus {
-    case started, muted
+enum EngineStatus: String {
+    case started = "Заведен"
+    case muted = "Заглушен"
 }
 
 class Car{
-    var carBrand: String
-    var carColor: String
-    var yearRelease: Int
-    var mileage: Int
+    let carBrand: String
+    let carColor: String
+    let yearRelease: Int
+    let weightCar: Double // вес авто
     var engine: EngineStatus
-    
-    init(carBrand: String, carColor: String, yearRelease: Int, mileage: Int, engine: EngineStatus ) {
+    init(carBrand: String, carColor: String, yearRelease: Int, weightCar: Int, engine: EngineStatus ) {
         self.carBrand = carBrand
         self.carColor = carColor
         self.yearRelease = yearRelease
-        self.mileage = mileage
+        self.weightCar = Double(weightCar)
         self.engine = engine
     }
-   
 }
 
 class TrunkCar: Car {
-    
+
 }
 
 class SportСar: Car{
-    var overclockingTime: Double
+    let enginePower: Double
     var spoiler: SportCarSettings
-    
-    init(carBrand: String, carColor: String, yearRelease: Int, mileage: Int, overclockingTime: Double, engine: EngineStatus, spoiler: SportCarSettings ) {
-        self.overclockingTime = overclockingTime
+    init(carBrand: String, carColor: String, yearRelease: Int, weightCar: Int, engine: EngineStatus, spoiler: SportCarSettings, enginePower: Int) {
         self.spoiler = spoiler
-        super.init(carBrand: carBrand, carColor: carColor, yearRelease: yearRelease, mileage: mileage, engine: engine)
+        self.enginePower = Double(enginePower)
+        super.init(carBrand: carBrand, carColor: carColor, yearRelease: yearRelease, weightCar: weightCar, engine: engine)
     }
-    
+    func overclockingTime() -> String{
+        let greeting = weightCar/enginePower
+        let time = (greeting*100).rounded()/100
+        var messages: String = ""
+        if engine == .started{
+            messages = "Разгон от 0 до 100 км за \(time) секунд"
+        }else if engine == .muted {
+            messages = "Заведите машну!"
+        }
+        return messages
+    }
+    func printConsol(){
+        print("Марка авто - \(carBrand), Цвет авто - \(carColor), Год выпуска - \(yearRelease), Вес авто - \(weightCar) кг., Статус двигателя - \(engine.rawValue), Мощность двигателя - \(enginePower) лс., Статус сполера - \(spoiler.rawValue), \(overclockingTime())")
+    }
+    func SettingAuto (spoiler: SportCarSettings, engine: EngineStatus){
+        self.engine = engine
+        self.spoiler = spoiler
+    }
 }
+
+
+
+var lotus = SportСar(carBrand: "Lotus", carColor: "Белый", yearRelease: 2018, weightCar: 1550, engine: .started, spoiler: .raised, enginePower: 360)
+lotus.SettingAuto(spoiler: .retracted, engine: .muted)
+
+var ferari = SportСar(carBrand: "Ferari", carColor: "Красный", yearRelease: 2019, weightCar: 1750, engine: .started, spoiler: .raised, enginePower: 460)
+
+
+lotus.printConsol()
+ferari.printConsol()
